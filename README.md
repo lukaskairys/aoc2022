97ef249e9e80b910f0a31c435d4c9c7c8c47633a
lukaska <lukaska@wix.com>
2022-12-02 08:11:30 +0200

    solve day2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.43ms
-Time part 2: 0.583ms
-Both parts: 1.012595ms
+Time part 1: 0.633ms
+Time part 2: 1.254ms
+Both parts: 1.887794ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.356ms
+Time part 2: 0.425ms
+Both parts: 0.781362ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 1.013ms
+Total stars: 4/50
+Total time: 2.669ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.ts
@@ -0,0 +1,66 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  enum scoreTable {
+    "A X" = 4,
+    "A Y"= 8,
+    "A Z"= 3,
+    "B X"= 1,
+    "B Y"= 5,
+    "B Z"= 9,
+    "C X"= 7,
+    "C Y"= 2,
+    "C Z"= 6,
+  }
+
+  return input.split(/\n/).map((game:string) => scoreTable[game as keyof typeof scoreTable]).reduce((a, b) => a + b, 0);
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  const scoreTable = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
+  }
+
+  return input.split(/\n/).map(game => scoreTable[game as keyof typeof scoreTable]).reduce((a, b) => a + b, 0);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `A Y
+B X
+C Z`,
+        expected: 15,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `A Y
+B X
+C Z`,
+        expected: 12,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
