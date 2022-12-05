52a1ad6a884bcd6c8d5bf44bd1368f103fa154a2
lukaska <lukaska@wix.com>
2022-12-05 09:12:27 +0200

    solve day 5

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
-![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -90,16 +90,16 @@ Both parts: 0.783268ms
 
 ```
 Day 04
-Time part 1: 2.274ms
-Time part 2: 2.491ms
-Both parts: 4.765413ms
+Time part 1: 2.304ms
+Time part 2: 1.814ms
+Both parts: 4.118734ms
 ```
 
 ```
 Day 05
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.029ms
+Time part 2: 233.127ms
+Both parts: 235.156128ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 8.288ms
+Total stars: 10/50
+Total time: 242.798ms
 ```
 
 <!--/RESULTS-->
--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,38 +1,73 @@
 import run from "aocrunner";
 
+const parseLine = (line: string) => line.trim().substring(5).split(" from ").map((x) => x.split(" to ")).flatMap((x) => x.map((y) => parseInt(y)))
+
 const part1 = (input: string) => {
 
-  return;
+  const stacks = {
+    1: ["Q" , "M" , "G" , "C" , "L"],
+    2: ["R" , "D" , "L" , "C" , "T" , "F" , "H", "G"],
+    3: ["V" , "J" , "F" , "N" , "M" , "T" , "W", "R"],
+    4: ["J" , "F" , "D" , "V" , "Q" , "P"],
+    5: ["N" , "F" , "M" , "S" , "L" , "B" , "T"],
+    6: ["R" , "N" , "V" , "H" , "C" , "D" , "P"],
+    7: ["H" , "C" , "T"],
+    8: ["G" , "S" , "J" , "V" , "Z" , "N" , "H", "P"],
+    9: ["Z" , "F" , "H" , "G"]
+  }
+
+  for ( let line of input.split(/\n/) ) {
+
+    const [qty, from, to] = parseLine(line)
+
+    for (let i = 0; i < qty; i++) {
+      stacks[to].push(stacks[from].pop())
+    }
+  }
+
+  const resultArr = []
+
+  for ( let stack in stacks ) {
+    resultArr.push(stacks[stack].slice(-1)[0])
+  }
+
+  return resultArr.join("");
 };
 
 const part2 = (input: string) => {
 
-  return;
-};
+  const stacks = {
+    1: ["Q" , "M" , "G" , "C" , "L"],
+    2: ["R" , "D" , "L" , "C" , "T" , "F" , "H", "G"],
+    3: ["V" , "J" , "F" , "N" , "M" , "T" , "W", "R"],
+    4: ["J" , "F" , "D" , "V" , "Q" , "P"],
+    5: ["N" , "F" , "M" , "S" , "L" , "B" , "T"],
+    6: ["R" , "N" , "V" , "H" , "C" , "D" , "P"],
+    7: ["H" , "C" , "T"],
+    8: ["G" , "S" , "J" , "V" , "Z" , "N" , "H", "P"],
+    9: ["Z" , "F" , "H" , "G"]
+  }
 
-const testInput = ``
-const firstTestResult = ""
-const secondTestResult = ""
+  for ( let line of input.trim().split(/\n/) ) {
+
+    const [qty, from, to] = parseLine(line)
+    stacks[to].push(...stacks[from].splice(-qty))
+  }
+
+  const resultArr = []
+
+  for ( let stack in stacks ) {
+    resultArr.push(stacks[stack].slice(-1)[0])
+  }
+
+  return resultArr.join("");
+};
 
 run({
   part1: {
-    tests: [
-      {
-        input: testInput,
-        expected: firstTestResult,
-      },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      {
-        input: testInput,
-        expected: secondTestResult,
-      },
-    ],
     solution: part2,
   },
-  trimTestInputs: true,
-  onlyTests: false,
 });
